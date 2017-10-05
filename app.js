fbb671ba92e4c59bb051223cddfa9a60f981367b
Cam Scherman <camscherman@hotmail.com>
2017-10-05 14:04:24 -0700

    replace custom middleware with morgan

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const Express = require('express')
+const morgan = require('morgan')
 
 const app = Express()
 
-app.use((req, res, next) => {
-    const {method,path} = req;
-    console.log(`Request with method ${method} to ${path}  at ${new Date()}.`)
-    next();
-})
+// app.use((req, res, next) => {
+    // const {method,path} = req;
+    // console.log(`Request with method ${method} to ${path}  at ${new Date()}.`)
+    // next();
+// })
+
+app.use(morgan('dev'));
 
 app.get('/', (req,res) =>{
     res.send("Hello, World");
