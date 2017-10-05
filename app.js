c6fe89f046ed3ed8a037932f60a6f999e92d3647
Cam Scherman <camscherman@hotmail.com>
2017-10-05 13:58:04 -0700

    Add custom middleware

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const Express = require('express')
 
 const app = Express()
 
+app.use((req, res, next) => {
+    const {method,path} = req;
+    console.log(`Request with method ${method} to ${path}  at ${new Date()}.`)
+    next();
+})
+
 app.get('/', (req,res) =>{
     res.send("Hello, World");
 })
